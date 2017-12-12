36b90159eda6c84bd152270cba038f1a86f3b614
avit sidis <avit.sidis@gmail.com>
2017-12-12 11:20:49 +0100

    Add specific text on About in french and canadian french

--- Samples.MultiCulture.MvcApplication/Global.asax.cs
+++ Samples.MultiCulture.MvcApplication/Global.asax.cs
@@ -17,5 +17,6 @@ namespace Samples.MultiCulture.MvcApplication
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
     }
 }
